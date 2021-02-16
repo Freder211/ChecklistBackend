66a0bbf53a452de2cc22c57ae2e1229f2c47eb07
Federico Di Filitto <f.difilitto@gmail.com>
2021-02-16 15:02:19 +0100

    Created test view

--- checklist/views.py
+++ checklist/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+def test(request):
+    # if this is a POST request we need to process the form data
+    return render(request, 'checklist/test.html', {})
+
